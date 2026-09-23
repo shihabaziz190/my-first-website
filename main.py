2946cbb03530dca2da9451932a638c076663db7d
Shihab Uddin <shihabaziz57@gmail.com>
2026-09-23 19:07:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.secret_key = SECRET_KEY
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from functools import wraps
 
-app = Flask(__name__)
+app = Flask(__name__hadi_electro_mart)
 app.secret_key = 'super_secret_key_change_in_production'
 
 ADMIN_EMAIL = "admin@gmail.com"
